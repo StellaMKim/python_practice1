873fd64649645fc2d6b9c104e7b8c1d6ea015091
StellaMKim <popscene7171@gmail.com>
2023-03-24 12:08:00 +0900

    python basic2

---
+++ myvenv/Chaptor3/01.number,character,boolean.py
@@ -0,0 +1,21 @@
+# comments
+# 1. add comments
+# 2. without comments
+
+# - number
+#1. integer : without decimal point
+print(1, 3 , 0, -1, sep="")
+
+#2. real number : with decimal point
+print(1.1, 3.1, 0, -1.4)
+
+# - character
+# "" or ''
+print("It is very good.", end='')
+print("Python is very interesting!")
+print('He shouted,"The dog is so noisy!"')
+
+# - boolean
+# True or False
+print(True)
+print(False)
